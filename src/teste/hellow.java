909478820ab4fc683b6fbfe34ccb92d9debe2193
chris <nnublada@gmail.com>
2024-02-08 16:37:29 -0300

    sss

--- src/teste/hellow.java
+++ src/teste/hellow.java
@@ -2,6 +2,6 @@ package teste;
 
 public class hellow {
 	public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+        System.out.println("Hello, World! ssssssssssssssssss"); 
     }
 }
